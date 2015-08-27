57e3a2b89f1adc6bcee8c6cdfa2ae049cd9e3153
Lester B. Hurtado <lester@applester.co>
2015-08-27 19:09:55 +0800

    Install PHP client library for Telerivet REST API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,24 @@
 |
 */
 
+require_once "/home/vagrant/Code/txtcmdr/vendor/telerivet/telerivet-php-client/telerivet.php";
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+get('info', function () {
+    $API_KEY = 'rnZKLkpBb3396YspUH9PReFRy4lh4bFw';           // from https://telerivet.com/api/keys
+    $PROJECT_ID = 'PJf3e398e4fb9f4a07';
+
+    $telerivet = new Telerivet_API($API_KEY);
+
+    $project = $telerivet->initProjectById($PROJECT_ID);
+
+// Send a SMS message
+    $project->sendMessage(array(
+        'to_number' => '+639189362340',
+        'content' => 'Hello world!'
+    ));
+    phpinfo();
 });
