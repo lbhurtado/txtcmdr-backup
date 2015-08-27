5f75a7b52b593b2bcb76d0147631ff527d1933d8
Lester B. Hurtado <lester@applester.co>
2015-08-27 19:28:33 +0800

    Test Telerivet APIs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+get('test', function () {
+    return 'test';
+});
+
 get('info', function () {
     $API_KEY = 'rnZKLkpBb3396YspUH9PReFRy4lh4bFw';           // from https://telerivet.com/api/keys
     $PROJECT_ID = 'PJf3e398e4fb9f4a07';
@@ -28,7 +32,49 @@ get('info', function () {
 // Send a SMS message
     $project->sendMessage(array(
         'to_number' => '+639189362340',
-        'content' => 'Hello world!'
+        'content' => 'Hello world - testing 1,2,3,4!'
     ));
+
+    // Query contacts
+    $name_prefix = 'Lester';
+    $cursor = $project->queryContacts(array(
+        'name[prefix]' => $name_prefix,
+        'sort' => 'name',
+    ))->limit(20);
+
+    echo "{$cursor->count()} contacts matching $name_prefix:\n";
+
+    while ($cursor->hasNext())
+    {
+        $contact = $cursor->next();
+        echo "{$contact->name} {$contact->phone_number} {$contact->vars->birthdate}\n";
+    }
+
+    // Import a contact
+    $contact = $project->getOrCreateContact(array(
+        'name' => 'John Smith',
+        'phone_number' => '555-0001',
+        'vars' => array(
+            'birthdate' => '1981-03-04',
+            'network' => 'Vodacom'
+        )
+    ));
+
+    // Add a contact to a group
+    $group = $project->getOrCreateGroup('Subscribers');
+    $contact->addToGroup($group);
+
+    $table = $project->getOrCreateDataTable("pop");
+
+    $row = $table->createRow(array(
+        'from_number' => "+639189362340",
+        'vars' => array(
+            'barangay' => "Philam",
+            'cluster_id' => 5,
+            'polling_place' => 'St. Vincent',
+            'precinct_id' => '0014A'
+        )
+    ));
+
     phpinfo();
 });
